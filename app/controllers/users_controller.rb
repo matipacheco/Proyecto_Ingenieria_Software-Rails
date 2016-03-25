15a48dd2c35b60b3454209391e3dd86f07550927
Mati <matias@cumplo.com>
2016-03-25 17:21:49 -0300

    Hice el login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user =  User.create(email: params[:user][:email], 
+                        password: params[:user][:password], 
+                        name: params[:user][:name], 
+                        surname: params[:user][:surname], 
+                        group_id: 4)
+    render 'users/login'
+  end
+
+  def login
+  	@user = User.find_by(email: params[:user][:email])
+    if @user && @user.password == params[:user][:password]
+      
+    elsif @user && @user.password != params[:user][:password]
+      @info = "Ususario o clave incorrecta, intentelo de nuevo."
+      render 'welcome/index'
+    else
+      render 'users/new'
+  	end
+  end
+
+  def register
+  end  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325172709) do
+ActiveRecord::Schema.define(version: 20160325194303) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160325172709) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.string   "name"
+    t.string   "surname"
   end
 
 end
