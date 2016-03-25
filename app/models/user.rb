d154ee1ff04a4b66b3238b9178e1a4d777da47b7
Mati <matias@cumplo.com>
2016-03-25 20:13:04 -0300

    Agrego nueva gema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   has_one :group
+
+  attr_accessible :name, :surname,
+  					      :email, :group_id
+  
+  def admin?
+  	group_id == 1
+  end
+
+  def editor?
+    group_id == 2
+  end
+
+  def privileged_user?
+    group_id == 3
+  end
+
+  def regular_user?
+    group_id == 4
+  end  
+
 end
