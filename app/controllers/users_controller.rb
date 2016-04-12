ebce0bf73788d620dbf36d3ee71b09f3d5ebcdfb
mati <matias@cumplo.com>
2016-04-12 12:58:42 -0300

    arreglando detalles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def new
     @user =  User.create(email: params[:user][:email], 
-                        password: params[:user][:password], 
                         name: params[:user][:name], 
                         surname: params[:user][:surname], 
                         group_id: 4)
-    render 'users/login'
-    #redirect_to root_url
+    @user.update_attribute(:password, params[:user][:password])
+    log_in @user
+    redirect_to root_url
   end
 
   def register
