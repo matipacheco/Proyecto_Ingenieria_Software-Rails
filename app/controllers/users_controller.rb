c001d09455de3c50bc71cad65749e4bac6e2a4e3
mati <matias@cumplo.com>
2016-04-22 16:17:30 -0300

    cambios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,23 @@ class UsersController < ApplicationController
   end
 
   def register
-  end  
+  end
+
+  def me
+  end
+
+  def update
+    user      = User.find params[:user_data][:user_id].to_i
+    password  = params[:user_data][:password]
+
+    unless password.nil?
+      user.update_attribute(:password, password)
+    end
+
+    redirect_to root_url
+  end
+
+  def create_editor
+    #primero elimiar el editaor anterior
+  end
 end
