814f13fc735e384e13200eb94e8923efbaf20604
mati <matias@cumplo.com>
2016-07-03 15:39:12 -0400

    migracion desde facebook

---
+++ migracion_facebook.rb
@@ -0,0 +1,148 @@
+f = FacebookFriend.new()
+f.update_attribute(:name, "Alex Patricio Santander Orellana")
+f.update_attribute(:email, "alexcarmelo@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Javiera Tapia Flores")
+f.update_attribute(:email, "javiera.josetf@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Viviana Carolina Morales Sánchez")
+f.update_attribute(:email, "vcmorales@musica.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Fabiola Harper")
+f.update_attribute(:email, "fabiolaharper@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Cristopher Alonso Guajardo Briceño")
+f.update_attribute(:email, "cristopherbenoit@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Jose Escobedo")
+f.update_attribute(:email, "jose.escobedo@hotmail.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Juan Pablo Orellana")
+f.update_attribute(:email, "chopchan@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Moro Gonzalez Chamberlain")
+f.update_attribute(:email, "waldogonz@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Llanco Barrera")
+f.update_attribute(:email, "llancobarrera@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Nacho Castillo Hewstone")
+f.update_attribute(:email, "ignacio.chewstone@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Marcos Stuardo")
+f.update_attribute(:email, "marcos.stuardo@kcl.ac.uk")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Karen Esther")
+f.update_attribute(:email, "kdonoso@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Ernesto Ándres Fernández León")
+f.update_attribute(:email, "fernaer82@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Rafael Francisco Díaz Ilva")
+f.update_attribute(:email, "rdiazs@uc.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Los-Chinganeros Cueca Chilena")
+f.update_attribute(:email, "luiscastrogonzalez@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Bastian Alexis Leon Muñoz")
+f.update_attribute(:email, "bastian_leon23@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Magaly Valdivia")
+f.update_attribute(:email, "magaly-valdivia@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Lilian Sepulveda" )
+f.update_attribute(:email, "lilibeth.musica@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Arias Verdejo Piojo Alvaro")
+f.update_attribute(:email, "piojorockpunk@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Acissey Castro Castro")
+f.update_attribute(:email, "katto_yee@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "JazzClub Antofagasta")
+f.update_attribute(:email, "clubdejazzantofagasta@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Felipe Gonzalez")
+f.update_attribute(:email, "felipe@gonzalezluthiers.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Erik Cuevas")
+f.update_attribute(:email, "basura.facebook1234@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Juan Nicolás Garces Barrientos")
+f.update_attribute(:email, "mte553@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Marcelo Alejandro Rodriguez Alvarez")
+f.update_attribute(:email, "marcelorodriguezalvarez@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Daniel Isaac Segundo Saavedra Castillo")
+f.update_attribute(:email, "danielisaac2dosaavedracastillo@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Chris J Ferj")
+f.update_attribute(:email, "undertheinfluence.-@live.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Retrosonios Rock")
+f.update_attribute(:email, "juan.gonzalezgo@usach.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Conjunto De Proyección Folclórica Nostalgias Campesinas")
+f.update_attribute(:email, "nostalgias_campesinas@hotmail.cl")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Estudiantina Victoria")
+f.update_attribute(:email, "estudiantinavictoria@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Alapp Sede Sur")
+f.update_attribute(:email, "extensionalappsur@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Ezequiel Troncoso")
+f.update_attribute(:email, "eze.20@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Los Diamantes De La Cumbia II")
+f.update_attribute(:email, "diamantes2013_2014@hotmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Espacio Arte Educación Perfil Lleno")
+f.update_attribute(:email, "espacio.arte.educacion@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Rey De Copas")
+f.update_attribute(:email, "gruporeydecopas@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Los Titanes De La Cumbia Queilen Chiloe")
+f.update_attribute(:email, "lostitanes.queilen@gmail.com")
+f.save
+f = FacebookFriend.new()
+f.update_attribute(:name, "Gerardo Marcoleta")
+f.update_attribute(:email, "marcoletagerardo@gmail.com")
+f.save
